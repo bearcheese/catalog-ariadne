370bd3458b8ca34100862ab1a65f78f1bbae24ec
bearcheese <bearcheese@gmail.com>
2011-05-15 00:14:02 +0200

    remove old dirs

--- common/src/main/java/hu/bearmaster/phoenix/common/dao/CategoryDAO.java
+++
@@ -1,9 +0,0 @@
-package hu.bearmaster.phoenix.common.dao;
-
-import hu.bearmaster.phoenix.common.model.Category;
-
-public interface CategoryDAO extends GenericDAO<Category, Long> {
-
-	Category findByName(String name);
-	
-}
--- common/src/main/java/hu/bearmaster/phoenix/common/dao/TypeDAO.java
+++
@@ -1,9 +0,0 @@
-package hu.bearmaster.phoenix.common.dao;
-
-import hu.bearmaster.phoenix.common.model.Type;
-
-public interface TypeDAO extends GenericDAO<Type, Long> {
-
-	Type findByName(String name);
-	
-}
